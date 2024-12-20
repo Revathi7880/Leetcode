ab87ab3146e9459a19a7f08f561a837b79fb0f27
revathi_gollapudi <revathi7461@gmail.com>
2024-12-20 17:35:24 -0500

    12/20/2024

---
+++ ZigzagConversion.py
@@ -0,0 +1,28 @@
+def convert(s: str, numRows: int) -> str:
+    if numRows == 1 or numRows >= len(s):
+        return s
+    stringMatrix = [[] for _ in range(numRows)] 
+    i = 0
+    down = True
+    for char in s:
+        if i == 0:
+            down = True
+        elif i == numRows - 1:
+            down = False
+        stringMatrix[i].append(char)
+        if down:
+            i += 1
+        else:
+            i -= 1
+    
+    resultString = ''
+    for row in stringMatrix:
+        rowString = ''.join(row)
+        resultString += rowString
+    return resultString
+
+
+print(convert("PAYPALISHIRING"))
+
+
+    
