0aa87182e1a36411d29f39a1ba5111746f5a8cb3
revathi_gollapudi <revathi7461@gmail.com>
2025-01-06 23:59:44 -0500

    small changes

--- PascalsTriangleII.py
+++ PascalsTriangleII.py
@@ -3,14 +3,9 @@ def getRow(rowIndex: int):
     if rowIndex == 0:
         return pascalTriangle[0]
     for i in range(1,rowIndex + 1):
-        temp = []
-        for j in range(i+1):
-            if j == 0:
-                temp.append(1)
-            elif j == i:
-                temp.append(1)
-            else:
-                temp.append(pascalTriangle[i-1][j-1] + pascalTriangle[i-1][j])
+        temp = [1]*(i+1)
+        for j in range(1,i):
+            temp[j] = pascalTriangle[i-1][j-1] + pascalTriangle[i-1][j]
         pascalTriangle.append(temp)
         if i == rowIndex:
             return temp
--- PascalsTriangle_AnotherWay.py
+++ PascalsTriangle_AnotherWay.py
@@ -1,14 +1,9 @@
 def generate(numRows: int):
     pascalTriangle = [[1]]
     for i in range(1,numRows):
-        temp = []
-        for j in range(i+1):
-            if j == 0:
-                temp.append(1)
-            elif j == i:
-                temp.append(1)
-            else:
-                temp.append(pascalTriangle[i-1][j-1] + pascalTriangle[i-1][j])
+        temp = [1]*(i+1)
+        for j in range(1,i):
+            temp[j] = pascalTriangle[i-1][j-1] + pascalTriangle[i-1][j]
         pascalTriangle.append(temp)
     return pascalTriangle
 
